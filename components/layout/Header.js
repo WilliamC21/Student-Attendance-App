9715d74ceb062d0516a4a4f51a14e7ca2228b869
WilliamC21 <84275546+WilliamC21@users.noreply.github.com>
2021-11-16 22:19:03 +0000

    add logout button to header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,16 +1,24 @@
 import Link from "next/dist/client/link";
-import { Button } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import React from "react";
 
-import classes from "./Header.module.css";
+import Styles from "./Header.module.css";
 
 const Header = (props) => {
   return (
     <React.Fragment>
-      <header className={classes.header}>
-        <h1>EasyCheck</h1>
-        <h2>blah</h2>
-      </header>
+      <Flex className={Styles.header}>
+        <div>
+          <Link href="/">
+            <h1>EasyCheck</h1>
+          </Link>
+        </div>
+        <div className={Styles.right}>
+          <Link href="/">
+            <button className={Styles["logout-button"]}>Logout</button>
+          </Link>
+        </div>
+      </Flex>
     </React.Fragment>
   );
 };
