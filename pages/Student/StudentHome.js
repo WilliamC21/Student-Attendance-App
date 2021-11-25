76b5e3a7a0a20ca5efd751173d60cbaf5d950db6
WilliamC21 <84275546+WilliamC21@users.noreply.github.com>
2021-11-25 17:19:17 +0000

    glance, user data read@

--- pages/Student/StudentHome.js
+++ pages/Student/StudentHome.js
@@ -3,9 +3,10 @@ import Link from "next/dist/client/link";
 import Header from "../../components/layout/Header";
 import Styles from "./StudentHome.module.css";
 import HalfCard from "../../components/UI/HalfCard";
+import FeatureCard from "../../components/UI/FeatureCard";
 //import Button from "../../components/UI/Button";
 import { Flex, Container, VStack, HStack, Button } from "@chakra-ui/react";
-import { CircularProgressbar } from "react-circular-progressbar";
+import { CircularProgressbarWithChildren } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import { PrismaClient } from "@prisma/client";
 
@@ -14,19 +15,30 @@ const prisma = new PrismaClient();
 export async function getServerSideProps() {
   const student = await prisma.student.findUnique({
     where: {
-      id: 2,
+      id: 4,
+    },
+
+    include: {
+      enrolledInLecture: {},
+      gradesObtained: {},
     },
   });
 
   return {
     props: {
       student: student,
+      lectures: student.enrolledInLecture,
+      grades: student.gradesObtained,
     },
   };
 }
 
 export default function StudentHome(props) {
   let attendancePercentage = props.student.attendance;
+  let nextLecture = props.lectures[1];
+  let recentGrade = props.grades[0];
+
+  console.log(nextLecture);
 
   return (
     <React.Fragment>
@@ -55,16 +67,34 @@ export default function StudentHome(props) {
             <div className={Styles["glance-container"]}>
               <div className={Styles["glance-left"]}>
                 <h1>At a Glance</h1>
-                <br />
-                <p className={Styles["glance-text"]}>Your Next Class</p>
-                <br />
-                <p className={Styles["glance-text"]}>Your Most Recent Grade</p>
+
+                <h3 className={Styles["glance-text"]}>Your Next Class</h3>
+                <p>{nextLecture.lectureName}</p>
+
+                <h3 className={Styles["glance-text"]}>
+                  Your Most Recent Grade
+                </h3>
+                <p>
+                  {recentGrade.assessmentName} - {recentGrade.gradePercent}%
+                </p>
               </div>
               <div className={Styles["glance-left"]}>
-                <CircularProgressbar
+                <CircularProgressbarWithChildren
                   value={attendancePercentage}
                   text={`${attendancePercentage}%`}
-                />
+                  className={Styles["attendance-bar"]}
+                >
+                  <div
+                    style={{
+                      fontSize: 23,
+                      marginTop: 70,
+                      color: "white",
+                      pathColor: "#d6d6d6",
+                    }}
+                  >
+                    Your Attendance
+                  </div>
+                </CircularProgressbarWithChildren>
               </div>
             </div>
           </HalfCard>
